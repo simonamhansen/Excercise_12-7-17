bb58499328177ac2119555b112f7650c76fb4b25
simonamhansen <simonamhansen@hotmail.com>
2017-09-07 19:14:22 +0200

    Done

--- Excercise_12-7-17.R
+++ Excercise_12-7-17.R
@@ -28,3 +28,68 @@ summary(model3)
 
 # To make plot
 plot(fitted(model2), residuals(model2))
+
+# A plot to visualise Homoskedasticity
+plot(rnorm(100),rnorm(100))
+
+# To acess normality (maybe not that important)
+hist(residuals(model2))
+qqnorm(residuals(model2))
+
+# To check for influential datapoints
+dfbeta(model2)
+
+# To retrieve relevant libraries
+library(lme4)
+
+# To load data
+data= read.csv("http://www.bodowinter.com/tutorial/politeness_data.csv")
+summary(data)
+
+# To find missing data
+which(is.na(data)==T)
+
+# To make boxplot to examine random effect
+boxplot(frequency ~ attitude*gender,
+        col=c("white","lightgray"),data)
+
+# To make a mixed effect model
+politeness.model = lmer(frequency ~ attitude + (1|subject) + (1|scenario), data=data) 
+politeness.model
+
+politeness.model = lmer(frequency ~ attitude +
+                          gender + (1|subject) +
+                          (1|scenario), data=data)
+politeness.model
+
+
+#To check the significance of the model
+politeness.null = lmer(frequency ~ gender +
+                         (1|subject) + (1|scenario), data=data,
+                       REML=FALSE)
+politeness.null
+
+politeness.model = lmer(frequency ~ attitude +
+                          gender +
+                          (1|subject) + (1|scenario), data=data,
+                        REML=FALSE)
+
+anova(politeness.null, politeness.model)
+
+# To get coefficients from model
+coef(politeness.model)
+
+# To add random slope
+politeness.model = lmer(frequency ~ attitude +
+                          gender + (1+attitude|subject) +
+                          (1+attitude|scenario),
+                        data=data,
+                        REML=FALSE)
+coef(politeness.model)
+
+# To make null model and check signficance of model
+politeness.null = lmer(frequency ~ gender +
+                         (1+attitude|subject) + (1+attitude|scenario),
+                       data=data, REML=FALSE)
+
+anova(politeness.null, politeness.model)
